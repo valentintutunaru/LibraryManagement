18e7af2a3b0580ed584256fad869c0d0f264f27a
stoianandrei9090 <stoianandrei@yahoo.com>
2019-11-28 19:11:45 +0200

    Menu Better

--- src/main/java/ro/jademy/librarymgmt/MenuTest.java
+++ src/main/java/ro/jademy/librarymgmt/MenuTest.java
@@ -1,17 +1,14 @@
 package ro.jademy.librarymgmt;
-import java.lang.reflect.Method;
+
 
 public class MenuTest {
 
-    public static void method() {
-        int param = 12;
-        System.out.println("Method"+param);
-    }
+
 
     public static void main (String [] args) {
      Menu menuTest = new Menu("blalala");
      menuTest.setListOfOptions("Yes","No","Don't know","Maybe");
-     menuTest.setListOfMethods(" sdfsfs");
+
 
 
    //  menuTest.printMenu();
