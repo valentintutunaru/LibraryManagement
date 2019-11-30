18f9286d2b89176b340eb1ca77aa0b94b4ecafe6
bogdansava-github <bogdan.sava@me.com>
2019-11-30 20:02:38 +0200

    added a little more code to UserMenu

--- src/main/java/ro/jademy/librarymgmt/UserMenu.java
+++ src/main/java/ro/jademy/librarymgmt/UserMenu.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class UserMenu {
 
-    public static void userMenu() throws FileNotFoundException{
+    public static void userMenu() throws FileNotFoundException {
 
 //Creating arraylist of books from .CSV file
 
@@ -16,7 +16,74 @@ public class UserMenu {
         LibraryMenu.printUserMenu();
         System.out.print("Please choose an option (1-6): ");
 
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
+
+        while (option != 6) {
+
+            switch (option) {
+
+                case 1:
+
+                    //listing the book table
+                    System.out.println("Here is a list with all the books in the library:");
+                    ListBookTable.printTableBooks(148);
+                    PrintBooks.printBookTable(bookList);
+                    ListBookTable.printTableBooks(148);
+
+                    System.out.println();
+                    LibraryMenu.printManagerMenu();
+                    System.out.print("Please choose an option (1-6): ");
+                    option = scanner.nextInt();
+                    break;
+
+                //search for a book
+                case 2:
+                    System.out.print("Search for (title/author/genre/publisher/ISBN: ");
+                    String filter = scanner.next();
+
+                    ListBookTable.printTableBooks(148);
+                    PrintBooks.printBookTable(Library.searchBooks(filter, bookList));
+                    ListBookTable.printTableBooks(148);
+                    LibraryMenu.printManagerMenu();
+                    System.out.print("\nPlease choose an option (1-6):");
+                    option = scanner.nextInt();
+                    break;
+
+                //wishlist
+                case 3:
+
+                    break;
+
+                //book description
+                case 4:
+
+
+                    break;
+
+                //user details
+                case 5:
+
+
+                    break;
+
+                //exit
+                case 6:
+
+                    System.exit(0);
+                    break;
+
+
+                default:
+                    System.out.println("\nPlease choose an option (1-6): ");
+                    option = scanner.nextInt();
+
+
+            }
+        }
+
+
     }
+
 }
+
