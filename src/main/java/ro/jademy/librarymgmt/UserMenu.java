0c272abb61fdea7d28baa9b746ed6e9a6890af9c
bogdansava-github <bogdan.sava@me.com>
2019-11-30 19:53:10 +0200

    added user menu in UserMenu class

--- src/main/java/ro/jademy/librarymgmt/UserMenu.java
+++ src/main/java/ro/jademy/librarymgmt/UserMenu.java
@@ -1,12 +1,22 @@
 package ro.jademy.librarymgmt;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserMenu {
 
     public static void userMenu() throws FileNotFoundException{
 
+//Creating arraylist of books from .CSV file
 
+        File fileName = new File("onlineBooks_database.csv");
+        ArrayList<Book> bookList = ReadingBooks.readingBook(fileName);
+        LibraryMenu.printUserMenu();
+        System.out.print("Please choose an option (1-6): ");
 
+        Scanner scanner=new Scanner(System.in);
+        int option = scanner.nextInt();
     }
 }
