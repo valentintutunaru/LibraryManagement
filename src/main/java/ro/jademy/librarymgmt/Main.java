4f9a550813d172f08921635043fa7f6bf5013aee
Dragos Cosmin <dragos.cosmin1@gmail.com>
2019-12-01 19:43:20 +0200

    Adaugat clasa User cu metode: Reading/writing file logFile.csv, verifyUser, login, callFunction. In Main am introdus pe ramura Manager ID autetificare si adaugare.

--- src/main/java/ro/jademy/librarymgmt/Main.java
+++ src/main/java/ro/jademy/librarymgmt/Main.java
@@ -1,6 +1,8 @@
 package ro.jademy.librarymgmt;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -12,6 +14,9 @@ public class Main {
         System.out.println("\nWelcome to the Public Library");
         Scanner scanner = new Scanner(System.in);
         boolean menuExit=false;
+        File userFileName=new File("logFile.csv");
+        ArrayList<User> userList=User.readingUser(userFileName);
+
 
         while (!menuExit) {
         LibraryMenu.printLibraryMenu();
@@ -21,7 +26,43 @@ public class Main {
 
             switch (option) {
                 case 1:
-                    ManagerMenu.managerMenu();
+                    int count=3;
+                    System.out.println("Input your username: ");
+                    String userName=scanner.next();
+                    if (User.userVerify(userName, userList)){
+                        while (count>0){
+                        System.out.println("Input your password: ");
+                        String userPassword=scanner.next();
+                        if (User.login(userName,userPassword,userList)) {
+                            if (User.FunctionCall(userName, userPassword, userList).equalsIgnoreCase("librarian")){
+                                ManagerMenu.managerMenu();
+                                break;
+                            }
+                            System.out.println("Acces denied, your are not librarian! ");
+                            System.exit(0);
+
+
+                        } else {
+                            System.out.println("wrong password, please input your password: ");
+                            count--;
+                        }
+                        }
+                    } else {
+                        System.out.println("Inexistent user");
+                        System.out.println("Do you want to create new user? (Y/N)");
+                        String answer=scanner.next().toLowerCase();
+
+                        switch (answer){
+                            case "y":
+                                User.writingUser(userFileName,userList);
+                                break;
+                            case "n":
+                                System.exit(0);
+                            default:
+                                System.out.println("You must answer y for yes, n for no");
+                        }
+                    }
+
                     break;
 
                 case 2:
