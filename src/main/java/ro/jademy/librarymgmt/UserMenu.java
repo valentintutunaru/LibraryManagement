bf2a2ce0e76139b24b0c044f248153cc66ca6800
Dragos Cosmin <dragos.cosmin1@gmail.com>
2019-12-03 14:26:31 +0200

    create ReadWriteFile incorporating methods from ReadingBook, ReadingStock, ReadingUser, WritingStock.java Delete the redundant classes

--- src/main/java/ro/jademy/librarymgmt/UserMenu.java
+++ src/main/java/ro/jademy/librarymgmt/UserMenu.java
@@ -13,7 +13,7 @@ public class UserMenu {
         Scanner scanner = new Scanner(System.in);
         boolean menuExit = false;
         File fileName = new File("onlineBooks_database.csv");
-        ArrayList<Book> bookList = ReadingBooks.readingBook(fileName);
+        ArrayList<Book> bookList = ReadWriteFile.readingBook(fileName);
 
         while (!menuExit) {
 
